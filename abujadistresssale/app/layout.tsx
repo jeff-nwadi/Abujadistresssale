c9970fe6d1f2599e454b49992da06d4de23e8398
jefferson nwadi <jefftech108@gmail.com>
2025-10-31 16:52:18 +0100

    font changes

--- abujadistresssale/app/layout.tsx
+++ abujadistresssale/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Geist_Mono, DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const dm_sans = DM_Sans ({
+  variable : "--font-dm-sans",
+  subsets : ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "Abuja distress sales",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dm_sans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
