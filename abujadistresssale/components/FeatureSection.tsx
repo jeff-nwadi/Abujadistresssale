1e39bae971b16aa0f33aa95b46959befb84eb8fc
jefferson nwadi <jefftech108@gmail.com>
2025-10-31 16:25:05 +0100

    made ui changes

--- abujadistresssale/components/FeatureSection.tsx
+++ abujadistresssale/components/FeatureSection.tsx
@@ -14,22 +14,22 @@ export default function Feature1() {
         <ul className="grid gap-8 sm:grid-cols-2 lg:grid-cols-2">
           {/* Section 1 */}
           <li className="col-span-2 flex flex-col-reverse lg:flex-row-reverse gap-4">
-            <div className="bg-[#A2EE2F] p-8 md:p-12 rounded-3xl flex-1">
-              <h2 className="py-5 text-4xl sm:text-5xl font-medium">
-                Infusing creative alchemy into today's brands
+            <div className="bg-[#064420] p-8 md:p-12 rounded-3xl flex-1">
+              <h2 className="py-5 text-4xl sm:text-5xl font-medium text-[#FFD700]">
+                About us
               </h2>
-              <p className="pt-4 text-base sm:text-lg">
-                We increase brand visibility and awareness to attract new
-                customers through thoughtful storytelling and distinct,
-                adaptable communications strategies. With unmatched consumer
-                understanding and true collaboration, we bring brands'
-                authenticity and perspective to life.
+              <p className="pt-4 text-base sm:text-lg text-[#FFECB3]">
+                The Abuja distress sales is a one stop shop for quick, fast and credible trade and transactions. 
+                We priorities our customer’s preferences and submit trade secrets to ensure that the purpose and reason for the trade 
+                commodity is achieved and within the fastest time possible. Our focus is on verifiable items, 
+                and we confirm that we do our due diligence on each product to the comfort of the seller and the would be buyer.
+
               </p>
               <Link href="#" className="flex gap-3 items-center mt-10">
-                <div className="bg-white rounded-full p-2">
+                <div className="bg-[#FFECB3] rounded-full p-2">
                   <ArrowUpRight className="w-4 h-4 sm:w-5 sm:h-5 text-2xl" />
                 </div>
-                <p className="uppercase">Our services</p>
+                <p className="uppercase text-[#FFECB3]">Read more</p>
               </Link>
             </div>
             <div className="relative flex-1 h-[50vh] sm:h-[70vh] lg:h-[80vh]">
@@ -56,40 +56,44 @@ export default function Feature1() {
                 priority
               />
             </div>
-            <div className="bg-[#A2EE2F] p-8 md:p-12 rounded-3xl flex-1">
-              <h2 className="py-5 text-4xl sm:text-5xl font-medium">
-                Maximizing your orbit
+            <div className="bg-[#064420] p-8 md:p-12 rounded-3xl flex-1">
+              <h2 className="py-5 text-4xl sm:text-5xl font-medium text-[#FFD700]">
+                Services 
               </h2>
-              <p className="pt-4 text-base sm:text-lg">
-                We boost brand awareness through innovative storytelling and
-                adaptable strategies. Our deep consumer insight and collaborative
-                approach help brands showcase their most authentic selves.
+              <h2 className="text-[#fbe4a2]">Sales, Consultancy & Media</h2>
+              <p className="pt-4 text-base sm:text-lg text-[#FFECB3]">
+                We deliver powerful sales and consultancy solutions backed by strategic media execution. 
+                Our team helps brands grow by combining sharp business insight
+                 with compelling visuals and storytelling. 
+                Whether through expertly planned campaigns, stunning imagery, 
+                or engaging videos, we make your message impossible to ignore.
+                 From identifying new market opportunities to positioning your brand for success, 
+                 we turn ideas into measurable results and lasting connections.
               </p>
               <Link href="#" className="flex gap-3 items-center mt-10">
-                <div className="bg-white rounded-full p-2">
+                <div className="bg-[#FFECB3] rounded-full p-2 ">
                   <ArrowUpRight className="w-4 h-4 sm:w-5 sm:h-5 text-2xl" />
                 </div>
-                <p className="uppercase">Our services</p>
+                <p className="uppercase text-[#FFECB3]">Our services</p>
               </Link>
             </div>
           </li>
 
           {/* Section 3 */}
           <li className="col-span-2 flex flex-col-reverse lg:flex-row-reverse gap-4">
-            <div className="bg-[#A2EE2F] p-8 md:p-12 rounded-3xl flex-1">
-              <h2 className="py-5 text-4xl sm:text-5xl font-medium">
-                Designing stories that sell
+            <div className="bg-[#064420] p-8 md:p-12 rounded-3xl flex-1">
+              <h2 className="py-5 text-4xl sm:text-5xl font-medium text-[#FFD700]">
+                Contact us
               </h2>
-              <p className="pt-4 text-base sm:text-lg">
-                We connect brands to their audiences through strategy, creativity,
-                and seamless execution — driving both engagement and results.
+              <p className="pt-4 text-base sm:text-lg text-[#FFECB3]">
+                Address: No 2, kokoma close, wuse II, Abuja
               </p>
-              <Link href="#" className="flex gap-3 items-center mt-10">
-                <div className="bg-white rounded-full p-2">
-                  <ArrowUpRight className="w-4 h-4 sm:w-5 sm:h-5 text-2xl" />
-                </div>
-                <p className="uppercase">Our services</p>
-              </Link>
+               <p className="pt-4 text-base sm:text-lg text-[#FFECB3]">
+                Contact Phone Numbers: +2347010050028
+              </p>
+               <p className="pt-4 text-base sm:text-lg text-[#FFECB3]">
+                All social media handles: @abjdistresssales 
+              </p>
             </div>
             <div className="relative flex-1 h-[50vh] sm:h-[70vh] lg:h-[80vh]">
               <Image
--- abujadistresssale/components/Header.tsx
+++ abujadistresssale/components/Header.tsx
@@ -36,11 +36,11 @@ const Header = () => {
             {/* HERO TEXT */}
             <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-4 z-10">
                 <h1 className="text-3xl sm:text-5xl lg:text-7xl text-white font-semibold max-w-3xl">
-                Smart Investors Buy Distress
+                Where Strategy Meets Storytelling.
                 </h1>
 
                 <p className="text-white font-light mt-3 text-sm sm:text-base max-w-xl">
-                Secure luxury deals below market value before they disappear.
+              Sales, consultancy, and media crafted to elevate brands and spark real growth.
                 </p>
 
                 <Button className="mt-6 rounded-full hover:bg-[#A2EE2F]/80 bg-[#A2EE2F] text-[#243C00] font-semibold flex items-center gap-2 px-5 sm:px-7 py-3 sm:py-4 text-xs sm:text-base">
